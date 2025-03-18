06500f1cf0582244104daa3408cd63e709309c73
박진우 <tree6316mte@gmail.com>
2025-03-18 15:42:24 +0900

    <Feat> : "게임 씬 수정 및 연출과 연결"

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    public int chapterProgress = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        chapterProgress = PlayerPrefs.GetInt("chapterProgress", 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
